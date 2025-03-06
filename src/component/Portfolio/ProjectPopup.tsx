8c80375bbb6d0596e89283ce557e617b265ad55a
jaeyonging <wodyd1318@naver.com>
2025-03-06 18:31:22 +0900

    fix: css

--- src/component/Portfolio/ProjectPopup.tsx
+++ src/component/Portfolio/ProjectPopup.tsx
@@ -4,7 +4,7 @@ import Loading from '../../lotties/Loading';
 import { useQuery } from 'react-query';
 import { getProjectById } from '../../api/board';
 import TopLoading from '../../lotties/TopLoading';
-
+import '../../style/quill.snow.css'
 interface Props {
     pid: any;
     title: string;
@@ -13,15 +13,15 @@ interface Props {
 }
 
 const ProjectPopup = ({ pid, title, isOpen, onClose }: Props) => {
-    const {data, isLoading, isError, error} = useQuery(['getProjectById', pid], () => getProjectById(pid || ''))
-    if(isLoading) return <TopLoading/>
-    if(isError) throw error
+    const { data, isLoading, isError, error } = useQuery(['getProjectById', pid], () => getProjectById(pid || ''))
+    if (isLoading) return <TopLoading />
+    if (isError) throw error
 
     return (
 
         <PopupHeader title={title} isOpen={isOpen} onClose={onClose}>
             <div className='flex flex-col gap-2'>
-            <div dangerouslySetInnerHTML={{__html: data.content}}></div>
+                <div className='ql-editor bg-cardcolor text-white rounded-[10px]' dangerouslySetInnerHTML={{ __html: data.content }}></div>
             </div>
         </PopupHeader>
     )
